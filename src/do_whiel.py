b091331ab20094432a95eab65f9bae97d5e79d5f
Alex Chesters <alex.chesters@bbc.co.uk>
2017-05-30 14:15:26 +0100

    Do errors

--- src/do_whiel.py
+++ src/do_whiel.py
@@ -1,4 +1,8 @@
 def do_whiel(d0, wh1le):
+    if not callable(d0):
+        raise ValueError('d0 is no function!, D0 MUST BE A FUNCTION')
+    if not callable(wh1le) or type(wh1le) != bool:
+        raise ValueError('wh1le is no boolean or boolyfunc!, WH1LE MUST BE A BOOLEAN OR A BOOLYFUNC')
     while True:
         d0()
         if wh1le:
